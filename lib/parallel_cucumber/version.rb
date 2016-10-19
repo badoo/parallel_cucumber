c1a42731f70b23bf585c8be2c2946b914b8a13ba
Tim Baverstock <tim.baverstock@corp.badoo.com>
2016-10-19 16:59:27 +0100

    Make optional the 'pretty' formatter directed at stdout

--- lib/parallel_cucumber/version.rb
+++ lib/parallel_cucumber/version.rb
@@ -1,3 +1,3 @@
 module ParallelCucumber
-  VERSION = '0.2.0.pre.33'.freeze
+  VERSION = '0.2.0.pre.34'.freeze
 end
--- lib/parallel_cucumber/worker.rb
+++ lib/parallel_cucumber/worker.rb
@@ -28,6 +28,7 @@ module ParallelCucumber
       @cucumber_options = options[:cucumber_options]
       @test_command = options[:test_command]
       @pre_check = options[:pre_check]
+      @pretty = options[:pretty]
       @env_variables = options[:env_variables]
       @index = index
       @queue_connection_params = options[:queue_connection_params]
@@ -105,7 +106,7 @@ module ParallelCucumber
               FileUtils.rm_rf(test_batch_dir)
               FileUtils.mkpath(test_batch_dir)
               f = "#{test_batch_dir}/test_state.json"
-              cmd = "#{@test_command} --format pretty --format json --out #{f} #{@cucumber_options} "
+              cmd = "#{@test_command} #{@pretty} --format json --out #{f} #{@cucumber_options} "
               batch_env = {
                 :TEST_BATCH_ID.to_s => batch_id,
                 :TEST_BATCH_DIR.to_s => test_batch_dir,
