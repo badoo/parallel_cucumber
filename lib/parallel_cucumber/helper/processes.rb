20638f1826e79f7958a2cc5c5fd557556b8b19e2
Madhusudhan V <madhu.kumar@corp.badoo.com>
2017-04-04 12:13:31 +0100

    Removed sysproctable module

--- lib/parallel_cucumber/helper/processes.rb
+++ lib/parallel_cucumber/helper/processes.rb
@@ -1,6 +1,3 @@
-require 'sys/proctable'
-include Sys
-
 module ParallelCucumber
   module Helper
     module Processes
@@ -14,46 +11,25 @@ module ParallelCucumber
           end
         end
 
-        def ps_tree_windows
-          all_processes = ProcTable.ps
-          groups = all_processes.group_by{ |x| x[:ppid] }
-          groups.default = []
-
-          build_tree =
-            lambda do |parent|
-              [[parent[:pid], parent[:comm]], groups[parent[:pid]].map(&build_tree)]
-            end
-          build_tree
-        end
-
         def kill_tree(sig, root, tree = nil, old_tree = nil)
-          tree = ps_tree_windows
-          to_kill = []
-
-          tree[root][1].to_a.flatten.each { |p| to_kill << p if p.is_a?(Fixnum) }
-
-          to_kill.each do |p|
-            begin
-              Process.kill(Platform.windows? ? 9 : sig, p)
-            rescue Errno::ESRCH
-              nil # It's gone already? Hurrah!
+          if Platform.windows?
+            system("taskkill /pid #{root} /T")
+          else
+            descendants(root, tree, old_tree) do |pid|
+              begin
+                puts(sig, pid.to_i)
+                Process.kill(sig, pid.to_i)
+              rescue Errno::ESRCH
+                nil # It's gone already? Hurrah!
+              end
             end
           end
         end
-
 
         def all_pids_dead?(root, tree = nil, old_tree = nil)
-          all_processes = ProcTable.ps
-          all_processes.any? { |p| p.ppid.to_s == pid }
-=begin
-          if Platform.windows?
-            all_pids_dead_windows?
-          else
-            # Note: returns from THIS function as well as descendants: short-circuit evaluation.
-            descendants(root, tree, old_tree) { return false }
-            true
-          end
-=end
+          # Note: returns from THIS function as well as descendants: short-circuit evaluation.
+          descendants(root, tree, old_tree) { return false }
+          true
         end
 
         # Walks old_tree, and yields all processes (alive or dead) that match the pid, start time, and command in
