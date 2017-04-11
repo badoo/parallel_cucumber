646dfd887fe4a2152e42bb6c79477b3dff6ac1da
Madhusudhan V <madhu.kumar@corp.badoo.com>
2017-04-11 09:51:22 +0100

    Updated parallel cucumber changes

--- lib/parallel_cucumber/worker.rb
+++ lib/parallel_cucumber/worker.rb
@@ -177,12 +177,21 @@ module ParallelCucumber
                           # Copy files we might have renamed or moved
                           file_map.each do |user, worker|
                             unless worker == user
-                              cp_out = FileUtils.cp_r("#{worker}", "#{user}", verbose: true) #`cp -Rv #{worker} #{user} 2>&1`
+                              cp_out = if RUBY_PLATFORM =~ /mswin|mingw|migw32|cygwin|x64-mingw32/
+                                         `powershell cp #{worker} #{user} -recurse 2>&1`
+                                       else
+                                         `cp -Rv #{worker} #{user} 2>&1`
+                                       end
+
                               @logger.debug("Copy of #{worker} to #{user} said: #{cp_out}")
                             end
                           end
                           # Copy everything else too, in case it's interesting.
-                          cp_out = FileUtils.cp_r("#{test_batch_dir}/*", "#{@log_dir}", verbose: true) #`cp -Rv #{test_batch_dir}/*  #{@log_dir} 2>&1`
+                          cp_out = if RUBY_PLATFORM =~ /mswin|mingw|migw32|cygwin|x64-mingw32/
+                                     `powershell cp #{test_batch_dir}/*  #{@log_dir} -recurse 2>&1`
+                                   else
+                                     `cp -Rv #{test_batch_dir}/*  #{@log_dir} 2>&1`
+                                   end
                           @logger.debug("Copy of #{test_batch_dir}/* to #{@log_dir} said: #{cp_out}")
                           parse_results(test_state)
                         end
