f99f7039b5a70dde6d9210b957bf501f3f25c3c2
Vyacheslav Frolov <vyacheslav.frolov@corp.badoo.com>
2018-03-29 18:10:53 +0100

    Before batch hooks

--- lib/parallel_cucumber/hooks.rb
+++ lib/parallel_cucumber/hooks.rb
@@ -1,11 +1,23 @@
 module ParallelCucumber
   class Hooks
+    @before_batch_hooks ||= []
     @after_batch_hooks ||= []
 
     class << self
-      def register_after_batch( proc)
+      def register_before_batch(proc)
+        raise(ArgumentError, 'Please provide a valid callback') unless proc.respond_to?(:call)
+        @before_batch_hooks << proc
+      end
+
+      def register_after_batch(proc)
         raise(ArgumentError, 'Please provide a valid callback') unless proc.respond_to?(:call)
         @after_batch_hooks << proc
+      end
+
+      def fire_before_batch_hooks(*args)
+        @before_batch_hooks.each do |hook|
+          hook.call(*args)
+        end
       end
 
       def fire_after_batch_hooks(*args)
